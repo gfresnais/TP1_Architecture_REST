16bd0c3c960b052a56a41a1691e499f1be481c19
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-05-27 15:00:10 +0200

    Updated models and client

--- src/main/java/com/fr/ensim/architecture/tp01/Client/ClientREST.java
+++ src/main/java/com/fr/ensim/architecture/tp01/Client/ClientREST.java
@@ -3,14 +3,17 @@ package com.fr.ensim.architecture.tp01.Client;
 import com.fr.ensim.architecture.tp01.Model.Garantie;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Client REST (main)
+ */
 public class ClientREST {
     public static void main(String[] args) {
-        String url = "http://localhost:8080/";
+        String url = "http://localhost:8080/api/garantie";
 
         RestTemplate restTemplate = new RestTemplate();
 
         // Get all garanties
-        Garantie[] garanties = restTemplate.getForObject(url + "api/garantie", Garantie[].class);
+        Garantie[] garanties = restTemplate.getForObject(url, Garantie[].class);
 
         if( garanties != null ) {
             for (Garantie g:
@@ -20,17 +23,31 @@ public class ClientREST {
         }
 
         // Create a new Garantie object
-        Garantie garantie = new Garantie(42, "Garantie 42", 42, "La garantie 42");
-
-        // Put garantie
-        restTemplate.put(url + "api/garantie/{id}", garantie, garantie.getId());
-
-        // Get garantie
-        garantie = restTemplate.getForObject(url + "api/garantie/{id}", Garantie.class, 42);
+        Garantie garantie = restTemplate.postForObject(url + "?nom={nom}&montant={montant}&description={description}",
+                                                    null, Garantie.class,
+                                                "Garantie 42",
+                                                            42,
+                                                            "La garantie 42");
 
         if( garantie != null ) {
+            System.out.println(garantie);
+
+            // Update some values
+            garantie.setNom("Garantie toto");
+            garantie.setMontant(100);
+            garantie.setDescription("La garantie toto 100");
+            // Put garantie
+            restTemplate.put(url + "/{id}", garantie, garantie.getId());
+        }
+
+        // Get garantie
+        garantie = restTemplate.getForObject(url + "/{id}", Garantie.class, 42);
+
+        if( garantie != null ) {
+            System.out.println(garantie);
+
             // Delete
-            restTemplate.delete(url + "api/garantie/{id}", garantie, garantie.getId());
+            restTemplate.delete(url + "/{id}", garantie, garantie.getId());
         }
     }
 }
