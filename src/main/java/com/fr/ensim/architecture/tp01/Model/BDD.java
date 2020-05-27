267ffac45c9c82a03f02d53226bf275f252e9547
Gallien FRESNAIS <pro@gfresnais.xyz>
2020-05-27 14:13:07 +0200

    Added models

---
+++ src/main/java/com/fr/ensim/architecture/tp01/Model/BDD.java
@@ -0,0 +1,9 @@
+package com.fr.ensim.architecture.tp01.Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum BDD {
+    INSTANCE;
+    public static final Map<Integer, Garantie> bdd  = new HashMap<>();;
+}
---
+++ src/main/java/com/fr/ensim/architecture/tp01/Model/Garantie.java
@@ -0,0 +1,40 @@
+package com.fr.ensim.architecture.tp01.Model;
+
+public class Garantie {
+    private int id;
+    private String nom;
+    private int montant;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
